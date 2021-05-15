3d2cd69e5dfa4058caba153d31f0260bc94e108a
Ceren Yasa <cerenyasa97@hotmail.com>
2021-05-15 18:08:30 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_maps/data_handler/route_data.dart';
+import 'package:flutter_app_maps/models/marker_model.dart';
 import 'package:flutter_app_maps/google_maps_page/project_maps_view.dart';
+import 'package:provider/provider.dart';
+
+import 'google_maps_page/project_maps_viewmodel.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: ProjectMapsView()
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<GoogleMapsModel>(create: (context) => GoogleMapsModel()),
+          ChangeNotifierProvider<RouteData>(create: (context) => RouteData()),
+        ],
+        child: ProjectMaps(),
+      )
     );
   }
 }
