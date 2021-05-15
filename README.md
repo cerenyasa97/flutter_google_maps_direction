cb07aae161f5b80105b7685f00892e7ad5e62276
Ceren Yasa <cerenyasa97@hotmail.com>
2021-05-15 18:51:35 +0300

    Code refactoring commit

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Description
+Map methods were coded with Flutter and Google Maps API. Places API is used for autocomplete in address search. The predicted addresses are shown as a list for the user to choose. After the user determines the destination, a route is drawn with the "Draw Route" button. With the "Home" and "Work" buttons, the home and work locations that are statically given are displayed to the user.
+
+
+<p align="center">
+  <img src = "https://user-images.githubusercontent.com/59059790/118369623-b2945500-b5ac-11eb-8f57-7c9a79eda4f8.PNG" width = "500">
+</p>
+
+### Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+#### Prerequisites
+What things you need to install the software and how to install them
+* [Android Studio](https://developer.android.com/studio/install)
+* [Flutter SDK](https://flutter.dev/docs/get-started/install)
+
+Add the following packages under the "dependencies" tag in the pubspec.yaml file.
+* google_maps_flutter: ^ 2.0.3
+* geolocator: ^ 7.0.3
+* flutter_bloc: ^ 7.0.0
+* geocoding: ^ 2.0.0
+* http: ^ 0.13.1
+* flutter_polyline_points: ^ 1.0.0
+* provider: ^ 5.0.0
+
+#### Installing
+After making sure that the Android Studio and Flutter installations are complete, download the project and open it with Android Studio and start using it.
+
+### Getting Help
+Please report bugs (actually broken code, not usage questions) to the cerenyasa97/task_management_app_with_Flutter GitHub issue tracker, prefixing the issue name with "task_manager_bug".
+
+### Maintainer
+* Ceren Yaşa 
--- lib/google_maps_page/project_maps_view.dart
+++ lib/google_maps_page/project_maps_view.dart
@@ -15,17 +15,15 @@ class ProjectMaps extends StatefulWidget {
 
 // ignore: must_be_immutable
 class ProjectMapsView extends ProjectMapsViewModel {
-
   double height = 370;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: false,
         // Triggers the method that changes the camera position when FloatingActionButton is pressed
         floatingActionButton: floatingButtons(context),
-        body: _mapsBody
-    );
+        body: _mapsBody);
   }
 
   Widget floatingButtons(context) {
@@ -38,13 +36,17 @@ class ProjectMapsView extends ProjectMapsViewModel {
           label: Text("Home"),
           onPressed: () => goToHome(),
         ),
-        SizedBox(width: 5,),
+        SizedBox(
+          width: 5,
+        ),
         FloatingActionButton.extended(
           icon: Icon(Icons.work),
           label: Text("Work"),
           onPressed: () => goToWork(),
         ),
-        SizedBox(width: 5,),
+        SizedBox(
+          width: 5,
+        ),
         FloatingActionButton.extended(
           icon: Icon(Icons.done),
           label: Text("Draw Route"),
@@ -66,86 +68,104 @@ class ProjectMapsView extends ProjectMapsViewModel {
             case LocationStatus.FOUND:
               return SafeArea(
                   child: Stack(
-                  children: [
-                    Consumer<GoogleMapsModel>(
-                      builder: (context, mapModel, child) {
-                        return GoogleMap(
-                          zoomControlsEnabled: false,
-                          myLocationEnabled: true,
-                          mapType: MapType.normal,
-                          polylines: mapModel.polylineSet,
-                          initialCameraPosition:
-                          CameraPosition(target: currentLocation.latLong, zoom: 15),
-                          onMapCreated: (map) {
-                            mapController = map;
-                          },
-                          markers: mapModel.markerMap.isNotEmpty
-                              ? Set<Marker>.of(mapModel.markerMap.values)
-                              : Set<Marker>(),
+                children: [
+                  Consumer<GoogleMapsModel>(
+                    builder: (context, mapModel, child) {
+                      return GoogleMap(
+                        zoomControlsEnabled: false,
+                        myLocationEnabled: true,
+                        mapType: MapType.normal,
+                        polylines: mapModel.polylineSet,
+                        initialCameraPosition: CameraPosition(
+                            target: currentLocation.latLong, zoom: 15),
+                        onMapCreated: (map) {
+                          mapController = map;
+                        },
+                        markers: mapModel.markerMap.isNotEmpty
+                            ? Set<Marker>.of(mapModel.markerMap.values)
+                            : Set<Marker>(),
+                      );
+                    },
+                  ),
+                  Positioned(
+                    child: ProjectTextField(
+                      enabled: false,
+                      controller: locationController,
+                      onChanged: (text) {
+                        findPlace(text);
+                      },
+                    ),
+                    top: 80,
+                    left: 25,
+                    right: 25,
+                  ),
+                  Positioned(
+                    child: ProjectTextField(
+                      controller: destinationController,
+                      onChanged: (text) {
+                        findPlace(text);
+                        height = 370;
+                      },
+                    ),
+                    top: 140,
+                    left: 25,
+                    right: 25,
+                  ),
+                  Positioned(
+                    top: 190,
+                    left: 30,
+                    right: 30,
+                    child: ValueListenableBuilder<List<PlaceModel>>(
+                      valueListenable: listenablePlaceModels,
+                      builder: (context, predictionsList, child) {
+                        return Visibility(
+                          child: Builder(
+                              builder: (context) => Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(0.6),
+                                        backgroundBlendMode: BlendMode.darken),
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 5),
+                                    width: 370,
+                                    height: height,
+                                    child: ListView.separated(
+                                      itemBuilder: (context, index) {
+                                        return ListTile(
+                                          onTap: () {
+                                            getPlaceAddressDetails(
+                                                predictionsList[index].placeID);
+                                            setState(() {
+                                              height = 0;
+                                            });
+                                          },
+                                          title: Text(
+                                            predictionsList[index].mainText,
+                                            style: TextStyle(fontSize: 14, color: Colors.white),
+                                          ),
+                                          subtitle: Text(
+                                            predictionsList[index]
+                                                .secondaryText,
+                                            style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.white),
+                                          ),
+                                          leading: Icon(Icons.add_location, color: Colors.white,),
+                                        );
+                                      },
+                                      itemCount: predictionsList.length,
+                                      separatorBuilder: (context, index) {
+                                        return Divider(
+                                            height: 1, color: Colors.grey);
+                                      },
+                                    ),
+                                  )),
+                          visible: predictionsList.isNotEmpty,
                         );
                       },
                     ),
-                    Positioned(
-                      child: ProjectTextField(
-                        enabled: false,
-                        controller: locationController,
-                        onChanged: (text) {
-                          findPlace(text);
-                        },
-                      ),
-                      top: 80,
-                      left: 25,
-                      right: 25,
-                    ),
-                    Positioned(
-                      child: ProjectTextField(
-                        controller: destinationController,
-                        onChanged: (text) {
-                          findPlace(text);
-                          height = 370;
-                        },),
-                      top: 140,
-                      left: 25,
-                      right: 25,
-                    ),
-                    Positioned(
-                      top: 190,
-                      left: 30,
-                      right: 30,
-                      child: ValueListenableBuilder<List<PlaceModel>>(
-                        valueListenable: listenablePlaceModels,
-                        builder: (context, predictionsList, child){
-                          return  Visibility(
-                            child: Builder(builder: (context) => Container(
-                              padding: EdgeInsets.symmetric(horizontal: 5),
-                              width: 370,
-                              height: height,
-                              color: Colors.white,
-                              child: ListView.separated(itemBuilder: (context, index){
-                                return ListTile(
-                                  onTap: () {
-                                    getPlaceAddressDetails(predictionsList[index].placeID);
-                                    setState(() {
-                                      height = 0;
-                                    });
-                                  },
-                                  title: Text(predictionsList[index].mainText, style: TextStyle(fontSize: 14),),
-                                  subtitle: Text(predictionsList[index].secondaryText, style: TextStyle(fontSize: 12, color: Colors.blueGrey),),
-                                  leading: Icon(Icons.add_location),
-                                );
-                              },itemCount: predictionsList.length,
-                                separatorBuilder: (context, index){
-                                  return Divider(height: 1, color: Colors.grey);
-                                },),
-                            )),
-                            visible: predictionsList.isNotEmpty,
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                )
-              );
+                  ),
+                ],
+              ));
             default:
               return Center(
                 child:
